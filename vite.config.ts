e9693862c18cf66e6163c18d23e57cf834d1c168
santhosh-bs22 <santhosh220506@gmail.com>
2025-12-01 23:25:45 +0530

    portfolio added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolioreact',
   server: {
-    port: 3000,
+    host: true,
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  define: {
+    'global': 'window', // 👈 Fix: Add this line to define global
   },
 })
